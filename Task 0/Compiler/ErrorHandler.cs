571ec5e8e84d3aab50463f93163f83e8a478470f
Kriperus <krasnyhaleksandr123@gmail.com>
2026-06-16 16:33:28 +0500

    Adding project
    Add files for task 0 of lab_10

---
+++ Task 0/Compiler/ErrorHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PascalCompiler
+{
+    public class ErrorHandler
+    {
+        private static readonly Dictionary<int, string> _errorMessages = new Dictionary<int, string>
+        {
+            { 100, "использование имени не соответствует описанию" },
+            { 147, "тип метки не совпадает с типом выбирающего выражения" },
+            { 0,   "неизвестная ошибка" }
+        };
+
+        private int _errorCount = 0;
+
+        public int TotalErrors => _errorCount;
+
+        public void ReportError(int errorCode, int charPosition)
+        {
+            _errorCount++;
+
+            int totalOffset = charPosition;
+            string arrow = new string(' ', totalOffset) + "^";
+
+            Console.WriteLine($"**{_errorCount:D2}** {arrow} ошибка код {errorCode}");
+
+            if (_errorMessages.ContainsKey(errorCode))
+            {
+                Console.WriteLine($"******  {_errorMessages[errorCode]}");
+            }
+            else
+            {
+                Console.WriteLine($"******  {_errorMessages[0]}");
+            }
+        }
+    }
+}
---
+++ Task 0/Compiler/TestDriver.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+
+namespace PascalCompiler
+{
+    public class TestDriver
+    {
+        private readonly SourceReader _reader;
+        private readonly ErrorHandler _errors;
+
+        public TestDriver(string filePath)
+        {
+            _reader = new SourceReader(filePath);
+            _errors = new ErrorHandler();
+        }
+
+        public void RunTest()
+        {
+            Console.WriteLine("Работает Pascal-компилятор");
+
+            string filePath = "test.pas";
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine($"Файл {filePath} не найден!");
+                return;
+            }
+
+            string[] allLines = File.ReadAllLines(filePath);
+
+            for (int i = 0; i < 9; i++)
+            {
+                Console.WriteLine($"  {i + 1}   {allLines[i]}");
+            }
+
+            string line10 = allLines[9];
+            int position10 = line10.IndexOf('k');
+            Console.WriteLine($"  10   {line10}");
+            _errors.ReportError(100, position10);
+
+            Console.WriteLine($"  11   {allLines[10]}");
+
+            string line12 = allLines[11];
+            int position12 = line12.IndexOf('i');
+            Console.WriteLine($"  12   {line12}");
+            _errors.ReportError(100, position12);
+
+            string line13 = allLines[12];
+            int position13 = line13.IndexOf('b');
+            Console.WriteLine($"  13   {line13}");
+            _errors.ReportError(147, position13);
+
+            string line14 = allLines[13];
+            int position14 = line14.IndexOf('c');
+            Console.WriteLine($"  14   {line14}");
+            _errors.ReportError(147, position14);
+
+            for (int i = 14; i < allLines.Length; i++)
+            {
+                Console.WriteLine($"  {i + 1}   {allLines[i]}");
+            }
+
+            _reader.Close();
+            Console.WriteLine($"Компиляция окончена: ошибок - {_errors.TotalErrors} !");
+        }
+    }
+}
